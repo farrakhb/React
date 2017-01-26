731f56029aed377a3b43afdd8a0fb849fb94995a
Farrakh Baig <farrakh.baig@skybettingandgaming.com>
2017-01-26 15:25:54 +0000

    real time firebase db added, data linked and states updated

---
+++ 00 - Starter Files/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+
+  apiKey: "AIzaSyAp6gUhfTOoyoFH3jv3okpn_tRHGuMDK0o",
+  authDomain: "catch-of-the-day-fb-d85a7.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-fb-d85a7.firebaseio.com",
+});
+
+export default base;
--- 00 - Starter Files/src/components/App.js
+++ 00 - Starter Files/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base.js';
 
 class App extends React.Component{
     constructor(){
@@ -17,6 +18,20 @@ class App extends React.Component{
             order:{}
         };
     }
+
+    componentWillMount(){
+        this.ref =  base.syncState(`${this.props.params.storeId}/fishes`
+    ,{
+        context: this,
+        state: 'fishes'
+
+    });
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.ref);
+    }
+
 
     addFish(fish) {
         //update our state
